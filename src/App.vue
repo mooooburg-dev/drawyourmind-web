3cf3b2fed1229d66586aec820c57ab1254e0257d
jeongmu.park <jeongmupark@gmail.com>
2020-07-13 18:41:45 +0900

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App drawyourmind~~"/>
+    <HelloWorld msg="Welcome to Your Vue.js App with AWS S3"/>
   </div>
 </template>
 
