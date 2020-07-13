020bbc3f1fca70e814b16d1a845d3ed0d54f4a77
mooooburg-dev <jeongmupark@gmail.com>
2020-07-13 22:49:32 +0900

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App with AWS S3"/>
+    <HelloWorld msg="Welcome to Your Vue.js App with AWS S3 setting for HomeOS"/>
   </div>
 </template>
 
