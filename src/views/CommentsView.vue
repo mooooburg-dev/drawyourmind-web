12cc82a690b63e4c232404dd334c0d49b41f7060
mooooburg-dev <jeongmupark@gmail.com>
2020-07-19 18:23:51 +0900

    comments 추가

--- src/views/CommentsView.vue
+++ src/views/CommentsView.vue
@@ -3,9 +3,15 @@
     <div class="content-label">
       <span class="oxtail content-title">COMMENTS</span>
     </div>
-    <div class="content-container career-list">
-      <div v-for="(item, item_idx) in careers" :key="item_idx" class="career-item">
-        <span class="career-text oxtail">{{ item.name }}</span>
+    <div class="content-container career-list w-100">
+      <div class="comment-form mr-2">
+        <b-form-input id="input-name" placeholder="Enter your name"></b-form-input>
+      </div>
+      <div class="comment-form mr-2" style="width:60%">
+        <b-form-input id="input-commet" placeholder="Enter your comment"></b-form-input>
+      </div>
+      <div>
+        <b-button variant="success">Submit</b-button>
       </div>
     </div>
   </div>
