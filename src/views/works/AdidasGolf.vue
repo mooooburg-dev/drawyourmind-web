66a6aad8f27cdb8d7883f44dac3dbde2a304575c
jeongmu.park <jeongmupark@gmail.com>
2020-07-31 18:46:58 +0900

    works 업데이트

--- src/views/works/AdidasGolf.vue
+++ src/views/works/AdidasGolf.vue
@@ -8,8 +8,22 @@
       <span>프로젝트 기간 : 2017.12 ~ 2018.12</span><br />
       <span>광고주 : 아디다스 코리아</span><br />
       <span>Role : 아디다스골프 온라인 마케팅 운영 대행</span><br />
-      <iframe class="youtube_content" width="840" height="473" src="https://www.youtube.com/embed/P_Oa-DLfRFs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
-      <iframe class="youtube_content" width="840" height="473" src="https://www.youtube.com/embed/SHU2E6fvWOw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/>
+      <div class="img-container text-center mt-5">
+        <ul>
+          <li>
+            <iframe class="youtube_content" width="840" height="473" src="https://www.youtube.com/embed/P_Oa-DLfRFs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </li>
+          <li>
+            <iframe class="youtube_content" width="840" height="473" src="https://www.youtube.com/embed/SHU2E6fvWOw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </li>
+          <li><img src="/static/img/adidas_golf_1.jpg" /></li>
+          <li><img src="/static/img/adidas_golf_2.jpg" /></li>
+          <li><img src="/static/img/adidas_golf_3.jpg" /></li>
+          <li><img src="/static/img/adidas_golf_4.jpg" /></li>
+          <li><img src="/static/img/adidas_golf_5.jpg" /></li>
+          <li><img src="/static/img/adidas_golf_6.jpg" /></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
