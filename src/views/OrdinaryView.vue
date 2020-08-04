f74f1205ecdacd264056be614987091cf33362f5
jeongmu.park <jeongmupark@gmail.com>
2020-08-04 16:13:23 +0900

    Ordinary 페이징 추가

--- src/views/OrdinaryView.vue
+++ src/views/OrdinaryView.vue
@@ -4,7 +4,7 @@
       <span class="oxtail content-title text-uppercase">{{ $route.name }}</span>
     </div>
     <div class="content-container ordinary-list">
-      <div v-for="(item, item_idx) in data" :key="item_idx" class="ordinary-item">
+      <div v-for="(item, item_idx) in paginatedData()" :key="item_idx" class="ordinary-item">
         <span class="ordinary-text oxtail"><a :href="item.url" target="_blank">{{ item.title }}</a></span>
         <span class="ordinary-sm-text oxtail"> at {{ $moment(item.date).format('MMMM YYYY') }}</span>
       </div>
@@ -16,6 +16,22 @@
       </b-button>
     </div>
      -->
+    <nav aria-label="Page navigation example">
+      <paginate
+        v-model="pageNum"
+        :page-count="pageCount()"
+        :page-range="5"
+        :margin-pages="1"
+        :container-class="'pagination justify-content-center'"
+        :prev-class="'page-item'" 
+        :page-class="'page-item'" 
+        :next-class="'page-item'"
+        :prev-link-class="'page-link'" 
+        :page-link-class="'page-link'" 
+        :next-link-class="'page-link'" >
+        :click-handler="clickCallback"
+      </paginate>
+    </nav>
   </div>
 </template>
 
@@ -27,6 +43,16 @@ export default {
     return {
       baseUrl: "",
       data: [],
+
+      pageNum: 1,
+    }
+  },
+
+  props: {
+    pageSize: {
+      type: Number,
+      required: false,
+      default: 10,
     }
   },
 
@@ -49,6 +75,26 @@ export default {
     },
 
     register() {},
+
+    pageCount(){
+      let listLength = this.data.length,
+        listSize = this.pageSize,
+        page = Math.floor(listLength / listSize);
+      if (listLength % listSize > 0) page += 1;
+
+      return page;
+    },
+
+    paginatedData(){
+      const start = (this.pageNum - 1) * this.pageSize,
+        end = start + this.pageSize;
+
+      return this.data.slice(start, end);
+    },
+
+    clickCallback(){
+      
+    },
   }
 }
 </script>
