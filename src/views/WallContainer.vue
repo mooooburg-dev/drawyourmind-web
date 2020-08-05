2969cd072d088c6577f0e631ee276a18cc4518bc
mooooburg-dev <jeongmupark@gmail.com>
2020-08-06 01:26:18 +0900

    fade 처리

--- src/views/WallContainer.vue
+++ src/views/WallContainer.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="wall-wrapper">
     <div class="wall-cover"></div>
-    <div v-if="issues.items.length > 0" class="issues-container">
-      
+    <transition name="fade">
+    <div v-if="issues.items && issues.items.length > 0" class="issues-container">
       <div class="mb-3"><img src="/static/img/logo_4.png" class="works-photo-icon"> Now Issues</div>
       <template v-for="(item, item_idx) in issues.items">
         <div v-if="!item.title.includes('Bump')" class="issue-item" :key="item_idx">
-          <a :href="item.html_url" target="_blank">{{item.title}} <span style="font-size:11px;">{{$moment(item.updated_at).format('YYYY/MM/DD')}}</span></a>
+          <a :href="item.html_url" target="_blank">{{item.title}} <span style="font-size:11px;">[{{getRepositoryNm(item.repository_url)}}] {{$moment(item.updated_at).format('YYYY/MM/DD')}}</span></a>
         </div>
       </template>
     </div>
+    </transition>
     <coding-video></coding-video>
   </div>
 </template>
@@ -51,11 +52,15 @@ export default {
       .catch(error => {
         console.log(error)
       })
+    },
+
+    getRepositoryNm(strUrl){
+      const repo_name = strUrl.split('mooooburg-dev/')[1];
+      return repo_name;
     }
   }
 }
 </script>
 
 <style>
-
 </style>
