bd704c9dcb1db3512b9ec4aa1408f4c2357b6162
mooooburg-dev <jeongmupark@gmail.com>
2020-08-09 20:44:08 +0900

    wallcontainer issues 'open'만 표시하기

--- src/views/WallContainer.vue
+++ src/views/WallContainer.vue
@@ -5,7 +5,7 @@
     <div v-if="issues.items && issues.items.length > 0" class="issues-container">
       <div class="issues-logo mb-3" style="cursor:pointer;" @click.prevent="issueLogoClick"><img src="/static/img/logo_4.png" class="works-photo-icon"> Now Issues</div>
       <template v-for="(item, item_idx) in issues.items">
-        <div v-if="!item.title.includes('Bump')" class="issue-item" :key="item_idx">
+        <div v-if="!item.title.includes('Bump') && item.state === 'open'" class="issue-item" :key="item_idx">
           <a :href="item.html_url" target="_blank">> {{item.title}} <span style="font-size:11px;">[{{getRepositoryNm(item.repository_url)}}] {{$moment(item.updated_at).format('YYYY/MM/DD')}}</span></a>
         </div>
       </template>
