4783119a69d5481484be1a81274900ef44d54f13
jeongmu.park <jeongmupark@gmail.com>
2020-08-21 17:21:55 +0900

    GOOUT STORE Operation

--- src/views/works/GooutStore.vue
+++ src/views/works/GooutStore.vue
@@ -13,6 +13,9 @@
       <span><img src="/static/img/logo_7.png" class="works-photo-icon"><a href="https://photos.app.goo.gl/AoEhN3KAGCK5m54Y6" target="_blank">GOOUT 활동 스케치</a></span>
       <div class="img-container text-center mt-5">
         <ul>
+          <li>
+            <iframe width="800" height="473" src="https://www.youtube.com/embed/NI-pz-a2AZc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </li>
           <li><img src="/static/img/works/goout_1.jpg" /></li>
         </ul>
       </div>
