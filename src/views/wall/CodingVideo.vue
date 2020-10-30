02bf63c51a3d3faea9fa3ed653338ad9a7661273
jeongmu.park <jeongmupark@gmail.com>
2020-10-30 16:33:12 +0900

    random video 추가

--- src/views/wall/CodingVideo.vue
+++ src/views/wall/CodingVideo.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="wall-content">
     <video autoplay muted loop id="myVideo">
-      <source src="/static/video/coding.mp4" type="video/mp4">
+      <source :src="getRandomVideoUrl()" type="video/mp4" />
     </video>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      
+      videoNum: 4,
+      videoPath: '/static/video/',
     }
-  }
+  },
+
+  methods: {
+    getRandomVideoUrl() {
+      const randomIdx = Math.ceil(Math.random() * this.videoNum - 1)
+      const videoPath = `${this.videoPath}video_${randomIdx}.mp4`
+      console.log(videoPath)
+      return videoPath
+    },
+  },
 }
 </script>
 
@@ -23,7 +33,7 @@ export default {
 }
 
 #myVideo {
-  opacity: .4;
+  opacity: 0.4;
   position: fixed;
   right: 0;
   bottom: 0;
