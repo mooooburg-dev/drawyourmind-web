b60846952c75ce11ea18b3865f2e97df5cdb847c
jeongmu.park <jeongmupark@gmail.com>
2020-11-26 12:20:14 +0900

    video 연속 재생

--- src/views/wall/CodingVideo.vue
+++ src/views/wall/CodingVideo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wall-content">
-    <video autoplay muted loop id="myVideo">
+    <video autoplay muted id="myVideo">
       <source :src="getRandomVideoUrl()" type="video/mp4" />
     </video>
   </div>
@@ -12,6 +12,18 @@ export default {
     return {
       videoNum: 4,
       videoPath: '/static/video/',
+    }
+  },
+
+  mounted() {
+    const video = document.querySelector('#myVideo')
+    const source = video.querySelector('source')
+    const self = this
+    video.onended = function() {
+      video.pause()
+      source.setAttribute('src', self.getRandomVideoUrl())
+      video.load()
+      video.play()
     }
   },
 
