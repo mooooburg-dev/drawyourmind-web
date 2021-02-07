857eb9aa433aa250e31f9c56855e0585f353da07
Jeongmu.Park <jeongmupark@gmail.com>
2021-02-07 21:58:46 +0900

    타임라인 구상 및 WallContainer 등록 (#16)

--- src/views/WallContainer.vue
+++ src/views/WallContainer.vue
@@ -42,6 +42,13 @@ export default {
     return {
       fixedIssues: [
         {
+          title: '타임라인 구상중',
+          html_url: 'https://www.notion.so/drawyourmind/Timeline-57f4ad0d1d1e40c2b1be068fe0fd09a0',
+          repository_url: 'drawyourmind-labs',
+          updated_at: '20210207',
+          visible: true,
+        },
+        {
           title: '📘 리액트를 다루는 기술(업데이트 중)',
           html_url: 'https://www.notion.so/drawyourmind/81dbc01da55e44ce9cda8db9054e7aab',
           repository_url: 'notion-BOOKS',
