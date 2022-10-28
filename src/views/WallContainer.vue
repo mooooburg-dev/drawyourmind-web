9acbda0211a96d6ec77248773a948df70d38028b
jeongmu.park <jeongmupark@gmail.com>
2022-10-28 16:25:43 +0900

    🔨 seperate fixed issues code

--- src/views/WallContainer.vue
+++ src/views/WallContainer.vue
@@ -49,6 +49,7 @@
 <script>
 import axios from 'axios'
 import CodingVideo from './wall/CodingVideo.vue'
+import fixedIssues from '../spread/issues'
 
 export default {
   components: {
@@ -57,31 +58,7 @@ export default {
 
   data() {
     return {
-      fixedIssues: [
-        {
-          title: 'drawyourmind-web repository',
-          html_url: 'https://github.com/mooooburg-dev/drawyourmind-web',
-          repository_url: 'drawyourmind-web',
-          updated_at: '20200712',
-          visible: true,
-        },
-        {
-          title: '타임라인 구상중',
-          html_url:
-            'https://www.notion.so/drawyourmind/Timeline-57f4ad0d1d1e40c2b1be068fe0fd09a0',
-          repository_url: 'drawyourmind-labs',
-          updated_at: '20210207',
-          visible: false,
-        },
-        {
-          title: '📘 리액트를 다루는 기술(업데이트 중)',
-          html_url:
-            'https://www.notion.so/drawyourmind/81dbc01da55e44ce9cda8db9054e7aab',
-          repository_url: 'notion-BOOKS',
-          updated_at: '20201116',
-          visible: false,
-        },
-      ],
+      fixedIssues,
       issues: {},
     }
   },
