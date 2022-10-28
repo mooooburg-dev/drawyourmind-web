bce15ce476678963ef8b55b7866ac4166f2f4931
jeongmu.park <jeongmupark@gmail.com>
2022-10-28 16:24:59 +0900

    🎨 clean format

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import axios from 'axios'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import Paginate from 'vuejs-paginate'
 
 Vue.use(require('vue-moment')) // Vus.use 한줄 더 심플하게~
 
 Vue.use(router)
-// Vue.use(axios)
+
 // Install BootstrapVue
 Vue.use(BootstrapVue)
+
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
 Vue.component('paginate', Paginate)
 
-// Vue.use(Bootstrap)
 Vue.config.productionTip = false
 
 new Vue({
