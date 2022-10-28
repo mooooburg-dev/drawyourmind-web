e03ee39fedcdc1fb039706246a7fc6ce40f265e2
jeongmu.park <jeongmupark@gmail.com>
2022-10-28 16:39:06 +0900

    🚨 lint fixed

--- src/views/works/AdidasGolf.vue
+++ src/views/works/AdidasGolf.vue
@@ -4,50 +4,85 @@
       <img src="/static/img/works/adidas_golf_0.jpg" />
     </div>
     <div class="work-item mt-4">
-      <span class="work-text oxtail">adidas golf marketing operation</span><br /><br />
+      <span class="work-text oxtail">adidas golf marketing operation</span
+      ><br /><br />
       <span>duration : 2017.12 ~ 2018.12</span><br />
       <span>Advertiser : 아디다스 코리아</span><br />
       <span>Member : MediaBling Crossmedia Team</span><br />
       <span>Role : 아디다스골프 온라인 마케팅 운영 대행</span><br />
       <br />
-      <span><img src="/static/img/logo_7.png" class="works-photo-icon"><a href="https://photos.app.goo.gl/PcXQB8TFHWXNdYmm9" target="_blank">프로젝트 스케치</a></span>
+      <span
+        ><img src="/static/img/logo_7.png" class="works-photo-icon" /><a
+          href="https://photos.app.goo.gl/PcXQB8TFHWXNdYmm9"
+          target="_blank"
+          >프로젝트 스케치</a
+        ></span
+      >
       <div class="img-container text-center mt-5">
         <ul>
           <li><img src="/static/img/works/adidas_golf_1.jpg" width="840" /></li>
           <li>
             <p class="text-left">
-              브랜드 마케팅 프로젝트로써 아디다스 골프의 연간 운영 매니징을 담당하였다. 소규모 인력으로 1년간 아디다스 코리아의 온라인 마켓팅을 운영함에 있어 결코 쉽지 않은 프로젝트였지만 아디다스 골프 현업 담당자 분들과 고군분투하며 나름대로 성공적으로 마무리 한 것 같은 프로젝트로 기억에 남는다.
+              브랜드 마케팅 프로젝트로써 아디다스 골프의 연간 운영 매니징을
+              담당하였다. 소규모 인력으로 1년간 아디다스 코리아의 온라인
+              마켓팅을 운영함에 있어 결코 쉽지 않은 프로젝트였지만 아디다스 골프
+              현업 담당자 분들과 고군분투하며 나름대로 성공적으로 마무리 한 것
+              같은 프로젝트로 기억에 남는다.
             </p>
           </li>
-          
+
           <li><img src="/static/img/works/adidas_golf_5.jpg" /></li>
           <li><img src="/static/img/works/adidas_golf_6.jpg" /></li>
           <li>
             <p class="text-left">
-              아디다스 골프의 마케팅 운영은 온라인에서 홍보용 목적을 지니고 기본적으로 활용할 수 있는 제품의 아이템 촬영부터 보다 다양한 영상 컨텐츠를 제작하였으며, 시즌에 맞는 키비쥬얼 촬영과 시즌 캠페인 영상 광고 역시 제작하였다. 컨텐츠 제작은 하우스 스튜디오의 프토그래퍼와 영상팀의 협업으로 진행 되었으며 당시 우리 크로스미디어팀에서 전체 관리를 담당하였다.
+              아디다스 골프의 마케팅 운영은 온라인에서 홍보용 목적을 지니고
+              기본적으로 활용할 수 있는 제품의 아이템 촬영부터 보다 다양한 영상
+              컨텐츠를 제작하였으며, 시즌에 맞는 키비쥬얼 촬영과 시즌 캠페인
+              영상 광고 역시 제작하였다. 컨텐츠 제작은 하우스 스튜디오의
+              프토그래퍼와 영상팀의 협업으로 진행 되었으며 당시 우리
+              크로스미디어팀에서 전체 관리를 담당하였다.
             </p>
           </li>
           <li>
-            <iframe class="youtube_content" width="840" height="473" src="https://www.youtube.com/embed/P_Oa-DLfRFs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              class="youtube_content"
+              width="840"
+              height="473"
+              src="https://www.youtube.com/embed/P_Oa-DLfRFs"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </li>
           <li>
-            <iframe class="youtube_content" width="840" height="473" src="https://www.youtube.com/embed/SHU2E6fvWOw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              class="youtube_content"
+              width="840"
+              height="473"
+              src="https://www.youtube.com/embed/SHU2E6fvWOw"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </li>
           <li>
             <p class="text-left">
-              가장 기억에 남는 메인 프로젝트는 19시즌 키비쥬얼 촬영이었는데 4명의 선수와 함께 2명씩 각각 두차례에 나눠서 진행되었다. 남해 사우스케이프와 포천의 푸른솔CC에서 진행 되었는데 사우스케이프의 경치와 풍경이 너무 좋았고 1박2일간 진행된 촬영에 남다른 열정을 보여준 스텝들이 기억에 남는다.
+              가장 기억에 남는 메인 프로젝트는 19시즌 키비쥬얼 촬영이었는데
+              4명의 선수와 함께 2명씩 각각 두차례에 나눠서 진행되었다. 남해
+              사우스케이프와 포천의 푸른솔CC에서 진행 되었는데 사우스케이프의
+              경치와 풍경이 너무 좋았고 1박2일간 진행된 촬영에 남다른 열정을
+              보여준 스텝들이 기억에 남는다.
             </p>
           </li>
-          
-          
+
           <li><img src="/static/img/works/adidas_golf_2.jpg" /></li>
           <li><img src="/static/img/works/adidas_golf_3.jpg" /></li>
           <li><img src="/static/img/works/adidas_golf_4.jpg" /></li>
-          
-          
+
           <li>
             <p class="text-left">
-              위 글에 대한 저작권은 본 사이트에 있으며 무단으로 복제 및 배포하실 수 없습니다.
+              위 글에 대한 저작권은 본 사이트에 있으며 무단으로 복제 및 배포하실
+              수 없습니다.
             </p>
           </li>
         </ul>
@@ -61,5 +96,3 @@ export default {}
 </script>
 
 <style></style>
-
-
--- src/views/works/DiorGallery.vue
+++ src/views/works/DiorGallery.vue
@@ -4,7 +4,8 @@
       <img src="/static/img/works/dior_0.jpg" />
     </div>
     <div class="work-item mt-4">
-      <span class="work-text oxtail">Dior VIP Customer Mobile Gallery</span><br /><br />
+      <span class="work-text oxtail">Dior VIP Customer Mobile Gallery</span
+      ><br /><br />
       <span>duration : 2017.12 ~ 2018.12</span><br />
       <span>Advertiser : Dior</span><br />
       <span>Member : MediaBling Crossmedia Team</span><br />
@@ -12,7 +13,14 @@
       <div class="img-container text-center mt-5">
         <ul>
           <li>
-            <iframe width="840" height="473" src="https://www.youtube.com/embed/BSNH6ISMebA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe
+              width="840"
+              height="473"
+              src="https://www.youtube.com/embed/BSNH6ISMebA"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </li>
           <li><img src="/static/img/works/dior_1.jpg" /></li>
           <li><img src="/static/img/works/dior_2.jpg" /></li>
