59e4629d7b01db5d423a9b50be9e6ed78e0f7010
jeongmu.park <jeongmupark@gmail.com>
2022-10-28 16:23:48 +0900

    🔧 update config code

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+modules.exports = {
+  globals: {
+    $: true,
+    jQuery: true,
+  },
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {},
+}
