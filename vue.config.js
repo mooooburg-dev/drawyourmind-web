ad417dd4e3455a0e41cf771ff650ebca3f5fde7b
jeongmu.park <jeongmupark@gmail.com>
2023-02-09 16:28:31 +0900

    🔧 webpack copy-webpack-plugin 추가

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,13 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   lintOnSave: true,
-  // pluginOptions: {
-  //   i18n: {
-  //     locale: "ko",
-  //     fallbackLocale: "ko",
-  //     localeDir: "locales",
-  //     enableInSFC: false
-  //   }
-  // },
   configureWebpack: {
-    // plugins: [
-    //   new webpack.ProvidePlugin({
-    //     'window.Quill': 'quill/dist/quill.js',
-    //     'Quill': 'quill/dist/quill.js'
-    //   }),
-    // ]
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: 'src/posts', to: 'static/posts' }],
+      }),
+    ],
     module: {
       rules: [
         {
