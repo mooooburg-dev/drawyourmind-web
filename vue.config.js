a15ac0892d86adab64140dd913aafb8cf5e6c154
jeongmu.park <jeongmupark@gmail.com>
2023-02-10 17:12:17 +0900

    🔧 vue config 초기화

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,7 @@
-const CopyPlugin = require('copy-webpack-plugin')
-
 module.exports = {
   lintOnSave: true,
   configureWebpack: {
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: 'src/posts', to: 'static/posts' }],
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.md$/i,
-          loader: 'raw-loader',
-        },
-      ],
-    },
+    plugins: [],
+    module: {},
   },
 }
