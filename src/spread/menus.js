49c5d7ec1a3516fea633e8c7e5e48a4dd7c98ee5
jeongmu.park <jeongmupark@gmail.com>
2023-02-12 13:31:58 +0900

    ⚡️ menu 데이터 분리 및 Daily365 경로 수정

---
+++ src/spread/menus.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Career',
+    path: '/career',
+    visible: true,
+  },
+  {
+    name: 'Works',
+    path: '/works',
+    visible: true,
+  },
+  {
+    name: 'Ordinary',
+    path: '/ordinary',
+    visible: true,
+  },
+  {
+    name: 'Daily365',
+    path: 'http://daily.drawyourmind.com/',
+    visible: true,
+  },
+  {
+    name: 'Dev.Note',
+    path: 'http://dev.drawyourmind.com/',
+    visible: true,
+  },
+  {
+    name: 'Blog',
+    path: 'https://blog.drawyourmind.com/',
+    visible: true,
+  },
+]
--- src/views/ProfileMenu.vue
+++ src/views/ProfileMenu.vue
@@ -77,42 +77,12 @@
 
 <script>
 import axios from 'axios'
+import menus from '../spread/menus'
+
 export default {
   data() {
     return {
-      menus: [
-        {
-          name: 'Career',
-          path: '/career',
-          visible: true,
-        },
-        {
-          name: 'Works',
-          path: '/works',
-          visible: true,
-        },
-        {
-          name: 'Ordinary',
-          path: '/ordinary',
-          visible: true,
-        },
-        {
-          name: 'Daily365',
-          path: 'https://daily.drawyourmind.com/',
-          visible: true,
-        },
-        {
-          name: 'Dev.Note',
-          path: 'http://dev.drawyourmind.com/',
-          visible: true,
-        },
-        {
-          name: 'Blog',
-          path: 'https://blog.drawyourmind.com/',
-          visible: true,
-        },
-      ],
-
+      menus,
       baseURL: '',
       tempPath: '',
 
