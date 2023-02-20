b5144fe4643c1a8e0d65cc1653384277e9deec75
jeongmu.park <jeongmupark@gmail.com>
2023-02-20 09:41:46 +0900

    🔥 사용하지 않는 이슈 삭제

--- src/spread/issues.js
+++ src/spread/issues.js
@@ -6,20 +6,4 @@ export default [
     updated_at: '20200712',
     visible: false,
   },
-  {
-    title: '타임라인 구상중',
-    html_url:
-      'https://www.notion.so/drawyourmind/Timeline-57f4ad0d1d1e40c2b1be068fe0fd09a0',
-    repository_url: 'drawyourmind-labs',
-    updated_at: '20210207',
-    visible: false,
-  },
-  {
-    title: '📘 리액트를 다루는 기술(업데이트 중)',
-    html_url:
-      'https://www.notion.so/drawyourmind/81dbc01da55e44ce9cda8db9054e7aab',
-    repository_url: 'notion-BOOKS',
-    updated_at: '20201116',
-    visible: false,
-  },
 ]
